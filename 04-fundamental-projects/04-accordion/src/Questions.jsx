f825b642b55d5ba1d8007d3c4d5201b010cbe4b3
Alexys Santiago <SuxPorT@hotmail.com>
2024-02-12 13:07:05 -0300

    feat: add #04 - Fundamental Projects - Accordion

---
+++ 04-fundamental-projects/04-accordion/src/Questions.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/prop-types */
+import SingleQuestion from './SingleQuestion';
+
+const Questions = ({ questions, activeId, toggleQuestion }) => {
+  return (
+    <section className='container'>
+      <h1>Questions</h1>
+      {questions.map((question) => {
+        return (
+          <SingleQuestion
+            key={question.id}
+            {...question}
+            activeId={activeId}
+            toggleQuestion={toggleQuestion}
+          />
+        );
+      })}
+    </section>
+  );
+};
+
+export default Questions;
