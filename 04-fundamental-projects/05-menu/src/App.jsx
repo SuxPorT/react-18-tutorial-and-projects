ec37e9e5cd830d69260d559b978ebea362ddadc9
Alexys Santiago <SuxPorT@hotmail.com>
2024-02-14 20:13:40 -0300

    feat: add #04 - Fundamental Projects - Menu

---
+++ 04-fundamental-projects/05-menu/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import Title from './Title';
+import items from './data';
+
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className='menu'>
+        <Title text='Our menu' />
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ 04-fundamental-projects/05-menu/src/Categories.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+const Categories = ({ categories, filterItems }) => {
+  return (
+    <div className='btn-container'>
+      {categories.map((category) => {
+        return (
+          <button
+            type='button'
+            className='btn'
+            key={category}
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Categories;
