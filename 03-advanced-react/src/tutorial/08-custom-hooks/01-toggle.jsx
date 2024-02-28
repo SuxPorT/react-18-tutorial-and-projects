4c2db9c1384409d40c850eb2b7cdaaad23afe3ac
Alexys Santiago <SuxPorT@hotmail.com>
2024-02-28 20:53:59 -0300

    feat: add #04 - Advanced React - Custom Hooks

---
+++ 03-advanced-react/src/tutorial/08-custom-hooks/01-toggle.jsx
@@ -0,0 +1,17 @@
+import useToggle from './useToggle';
+
+const ToggleExample = () => {
+  const { show, toggle } = useToggle(true);
+
+  return (
+    <div>
+      <h4>Toggle custom hook</h4>
+      <button className='btn' onClick={toggle}>
+        Toggle
+      </button>
+      {show && <h4>Some stuff</h4>}
+    </div>
+  );
+};
+
+export default ToggleExample;
