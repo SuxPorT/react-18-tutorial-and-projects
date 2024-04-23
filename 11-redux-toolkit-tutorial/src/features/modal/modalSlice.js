fa536c9a4c37001736a4a03d4e2f3060f482d8b8
Alexys Santiago <SuxPorT@hotmail.com>
2024-04-23 17:10:15 -0300

    feat: add #11 - Redux Toolkit Tutorial

---
+++ 11-redux-toolkit-tutorial/src/features/modal/modalSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  isOpen: false,
+};
+
+const modalSlice = createSlice({
+  name: 'modal',
+  initialState,
+  reducers: {
+    openModal: (state, _action) => {
+      state.isOpen = true;
+    },
+    closeModal: (state, _action) => {
+      state.isOpen = false;
+    },
+  },
+});
+
+export const { openModal, closeModal } = modalSlice.actions;
+
+export default modalSlice.reducer;
